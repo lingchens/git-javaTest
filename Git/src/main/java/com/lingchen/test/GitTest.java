5d5460702b7b4913e47be5475e8be24814628d42
lingchen <799728781@qq.com>
2020-05-22 22:39:41 +0800

    dd

--- Git/src/main/java/com/lingchen/test/GitTest.java
+++
@@ -1,8 +0,0 @@
-package com.lingchen.test;
-
-public class GitTest {
-    public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello");
-    }
-}
